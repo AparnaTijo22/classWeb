23b3ffcdf13c8b1b062b0767a4fcfdc9347990a8
AparnaTijo22 <aparnatijo22@gmail.com>
2023-10-27 16:42:16 +0530

    today27

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { PipeComponent } from './pipe/pipe.component';
     AppComponent,
     HomeComponent,
     ProductComponent,
-    PipeComponent
+    PipeComponent,
   ],
   imports: [
     BrowserModule
--- src/app/pipe/pipe.component.ts
+++ src/app/pipe/pipe.component.ts
@@ -1,3 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipe',
+  templateUrl: './pipe.component.html',
+  styleUrls: ['./pipe.component.css']
+})
 export class PipeComponent {
-  myName = 'Aparna Tijo';
+  myName = "Aparna Tijo";
+  currentDate = new Date();
+  emp = {name: "Alex", age: '25', address:{a1: 'Paris', a2: 'France'}};
 }
